3650cc6f5fac6d736319f68905106c87c0d3371d
mnair797 <66963709+mnair797@users.noreply.github.com>
2023-02-12 22:03:51 -0800

    Update cropyieldgcondensed.py

--- cropyieldgcondensed.py
+++ cropyieldgcondensed.py
@@ -1,3 +1,4 @@
+#Function launch_fe is acquired data from AWS
 def launch_fe(data):
     import os
     import pandas as pd
@@ -81,6 +82,7 @@ def launch_fe(data):
     train.to_csv('train.csv', index=False, header=False)
     test.to_csv('test.csv', index=False, header=False)
     column_names = list(train.columns)
+#End of acquired code
 def get_model_id():
     return "36c6bb80-5b2f-4d6a-8826-f169eea59b3e"
 
